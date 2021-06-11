74f0d51a524f32cd1bad5f671abf7a13b1377267
thomas-medelse <67781232+thomas-medelse@users.noreply.github.com>
2021-06-11 08:54:34 +0200

    Update Sf 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,8 +13,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('medelse_utm_cookie');
+        $treeBuilder = new TreeBuilder('medelse_utm_cookie');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
